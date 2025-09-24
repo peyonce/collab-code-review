82fa7b12f0943f329847e56c292db6e79b1cb30d
peyonce <nthabynthabie1@gmail.com>
2025-09-24 20:23:13 +0200

    fixed req

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,25 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import { errorMiddleware } from './middleware/errorMiddleware';
-import { defaultMaxListeners } from 'events';
 
 export const app: Application = express();
-
-
+  
 app.use(cors());
 app.use(helmet());
 app.use(express.json());
 app.use(morgan('dev'));
 
-
-app.get('/api/health', (req: Request, res: Response) => {
+ 
+app.get('/api/health', (_req, res: Response) => {
   res.json({ status: 'OK', message: 'Server is running' });
 });
 
- 
+
 app.use(errorMiddleware);
 
 
-app.use((req: Request, res: Response) => {
+app.use((_req, res) => {
   res.status(404).json({ error: 'Not Found' });
 });
-
-
